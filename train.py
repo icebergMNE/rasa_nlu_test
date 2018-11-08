feb6eac89fd6c9259190f7770f704dc8704a531d
stefan <fustarstefan@gmail.com>
2018-11-08 15:38:02 +0100

    first commit

---
+++ train.py
@@ -0,0 +1,8 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+
+training_data = load_data('data.md')
+trainer = Trainer(config.load("defaultConfig.yml"))
+trainer.train(training_data)
+model_directory = trainer.persist('./projects/100d')
